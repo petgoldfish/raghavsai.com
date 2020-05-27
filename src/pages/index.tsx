34bd83965ade206d9e20a292c4295c3c957e7f69
Raghav Sai <g.raghav.sai@gmail.com>
2020-05-27 14:52:58 -0400

    Updated landing page text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,14 +20,14 @@ const IndexPage: FC = () => {
 			/>
 			<h1>Hello World!</h1>
 			<p>
-				I'm currently living in New York City, pursuing a Master's in Computer
-				Science. Prior to this, I was a Full Stack Software Engineer at
-				PowerSchool.
+				I live in New York City, pursuing a Master's in Computer Science at NYU.
+				Before this, I was a Full Stack Software Engineer at PowerSchool in
+				Bangalore, India.
 			</p>
 			<p>
 				When I'm not playing around with Web Development or Programming
 				Languages, I'm usually playing video games or doing outdoorsy stuff like
-				traveling the world and hiking!
+				hiking and traveling the world (well, before the pandemic at least)!
 			</p>
 		</Layout>
 	);
