91b0cfec7999ea41ec61a39859129698aa1062ae
Raghav Sai <g.raghav.sai@gmail.com>
2020-06-05 16:53:46 -0400

    Made theme control keyboard accessible

--- src/components/ThemeControl/themeControl.tsx
+++ src/components/ThemeControl/themeControl.tsx
@@ -3,6 +3,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAdjust } from "@fortawesome/free-solid-svg-icons";
 import { ThemeToggler } from "gatsby-plugin-dark-mode";
 
+import "./themeControl.css";
+
 interface ThemeProps {
 	theme: string;
 	toggleTheme: (theme: string) => void;
@@ -12,14 +14,19 @@ export default function ThemeControl() {
 	return (
 		<ThemeToggler>
 			{({ theme, toggleTheme }: ThemeProps) => (
-				<FontAwesomeIcon
-					title="Toggle Theme"
-					icon={faAdjust}
-					width={18}
-					rotation={180}
-					cursor="pointer"
+				<a
+					href="#"
+					className="theme-control no-invert"
 					onClick={() => toggleTheme(theme === "dark" ? "light" : "dark")}
-				/>
+				>
+					<FontAwesomeIcon
+						title="Toggle Theme"
+						icon={faAdjust}
+						width={18}
+						rotation={180}
+						cursor="pointer"
+					/>
+				</a>
 			)}
 		</ThemeToggler>
 	);
