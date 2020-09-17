deb4a3a9d7065b63b33237153a324a8de80f796c
petgoldfish <petgoldfish@protonmail.com>
2020-09-17 19:12:54 -0400

    Add gpg key

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,46 +5,55 @@ import SEO from "../components/seo";
 import Social from "../components/social/social";
 
 function IndexPage() {
-	return (
-		<Layout>
-			<SEO
-				title="Home"
-				keywords={[
-					`raghav sai`,
-					`raghav`,
-					`sai`,
-					`blog`,
-					`gatsby`,
-					`javascript`,
-					`typescript`,
-					`react`,
-				]}
-			/>
-			<h1>Hello, world!</h1>
-			<p>
-				I live in New York City, and I'm pursuing a Master's in Computer Science at NYU.
-				Prior to this, I was a Full Stack Software Engineer at PowerSchool in
-				Bangalore, India.
-			</p>
-			<p>
-				When I'm not playing around with Web Development or Programming
-				Languages, I'm usually playing video games or doing outdoorsy stuff like
-				hiking and traveling the world (well, before the pandemic at least)!
-			</p>
-			<p>
-				Check out my{" "}
-				<a href="/resume" target="_blank" rel="noopener noreferrer">
-					résumé
-				</a>
-				!
-			</p>
-			<h2>Contact Me</h2>
-			<p>Email me at hi (at) raghavsai (dot) com.</p>
-			<p>
-				Find me on: <Social />
-			</p>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <SEO
+        title="Home"
+        keywords={[
+          `raghav sai`,
+          `raghav`,
+          `sai`,
+          `blog`,
+          `gatsby`,
+          `javascript`,
+          `typescript`,
+          `react`,
+        ]}
+      />
+      <h1>Hello, world!</h1>
+      <p>
+        I live in New York City, and I'm pursuing a Master's in Computer Science
+        at NYU. Prior to this, I was a Full Stack Software Engineer at
+        PowerSchool in Bangalore, India.
+      </p>
+      <p>
+        When I'm not playing around with Web Development or Programming
+        Languages, I'm usually playing video games or doing outdoorsy stuff like
+        hiking and traveling the world (well, before the pandemic at least)!
+      </p>
+      <p>
+        Check out my{" "}
+        <a href="/resume" target="_blank" rel="noopener noreferrer">
+          résumé
+        </a>
+        !
+      </p>
+      <h2>Contact Me</h2>
+      <p>Email me at hi (at) raghavsai (dot) com.</p>
+      <p>
+        My{" "}
+        <a href="/gpg-pubkey.txt" target="_blank" rel="noopener noreferrer">
+          GPG key
+        </a>{" "}
+        if you want to securely communicate with me. Fingerprint:{" "}
+        <pre>0B87F52A5DA252053ECB654A20E18C16307015FB</pre>
+      </p>
+      GPG key
+      <p>
+        Find me on: <Social />
+      </p>
+    </Layout>
+  );
 }
 
 export default IndexPage;
