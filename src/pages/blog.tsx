23d36e11125214ce2031ccbf5423145669c19203
petgoldfish <petgoldfish@protonmail.com>
2021-03-21 00:02:14 -0400

    Actually remove redundant 'blog' in path

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -7,56 +7,56 @@ import Bio from "../components/bio/bio";
 import { BlogPageQuery } from "../../graphql-types";
 
 interface BlogProps {
-	data: BlogPageQuery;
+  data: BlogPageQuery;
 }
 
 function BlogPage({ data }: BlogProps) {
-	const posts = data.allMdx.edges;
+  const posts = data.allMdx.edges;
 
-	return (
-		<Layout>
-			<SEO title="Blog" />
-			<Bio />
-			<div>
-				{posts.map(({ node }) => {
-					const title = node.frontmatter.title || node.fields.slug;
-					return (
-						<div key={node.fields.slug}>
-							<h2>
-								<Link to={`blog${node.fields.slug}`}>{title}</Link>
-							</h2>
-							<small>{node.frontmatter.date}</small>
-							<p
-								dangerouslySetInnerHTML={{
-									__html: node.frontmatter.description || node.excerpt,
-								}}
-							/>
-						</div>
-					);
-				})}
-			</div>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <SEO title="Blog" />
+      <Bio />
+      <div>
+        {posts.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug;
+          return (
+            <div key={node.fields.slug}>
+              <h2>
+                <Link to={`${node.fields.slug}`}>{title}</Link>
+              </h2>
+              <small>{node.frontmatter.date}</small>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </Layout>
+  );
 }
 
 export default BlogPage;
 
 export const pageQuery = graphql`
-	query blogPage {
-		allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
-			edges {
-				node {
-					excerpt
-					fields {
-						slug
-					}
-					frontmatter {
-						date(formatString: "MMMM DD, YYYY")
-						title
-						description
-					}
-				}
-			}
-		}
-	}
+  query blogPage {
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
 `;
