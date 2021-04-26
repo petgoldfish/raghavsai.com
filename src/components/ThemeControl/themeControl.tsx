6b6da749d255a65a4afed03ff0b9d31f528f279e
petgoldfish <petgoldfish@protonmail.com>
2021-04-26 19:59:25 -0400

    Switch to react icons

--- src/components/ThemeControl/themeControl.tsx
+++ src/components/ThemeControl/themeControl.tsx
@@ -1,33 +1,31 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAdjust } from "@fortawesome/free-solid-svg-icons";
+import { FaAdjust } from "react-icons/fa";
 import { ThemeToggler } from "gatsby-plugin-dark-mode";
 
 import "./themeControl.css";
 
 interface ThemeProps {
-	theme: string;
-	toggleTheme: (theme: string) => void;
+  theme: string;
+  toggleTheme: (theme: string) => void;
 }
 
 export default function ThemeControl() {
-	return (
-		<ThemeToggler>
-			{({ theme, toggleTheme }: ThemeProps) => (
-				<a
-					href="#"
-					className="theme-control no-invert"
-					onClick={() => toggleTheme(theme === "dark" ? "light" : "dark")}
-				>
-					<FontAwesomeIcon
-						title="Toggle Theme"
-						icon={faAdjust}
-						width={18}
-						rotation={180}
-						cursor="pointer"
-					/>
-				</a>
-			)}
-		</ThemeToggler>
-	);
+  return (
+    <ThemeToggler>
+      {({ theme, toggleTheme }: ThemeProps) => (
+        <a
+          href="#"
+          className="theme-control no-invert"
+          onClick={() => toggleTheme(theme === "dark" ? "light" : "dark")}
+        >
+          <FaAdjust
+            title="Toggle Theme"
+            width={18}
+            rotate={180}
+            cursor="pointer"
+          />
+        </a>
+      )}
+    </ThemeToggler>
+  );
 }
--- src/components/social/social.tsx
+++ src/components/social/social.tsx
@@ -1,58 +1,53 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-	faGithub,
-	faLinkedin,
-	faTwitter,
-} from "@fortawesome/free-brands-svg-icons";
+import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 import { useStaticQuery, graphql } from "gatsby";
 import { SocialQuery } from "../../../graphql-types";
 
 import "./social.css";
 
 export default function Social() {
-	const data: SocialQuery = useStaticQuery(graphql`
-		query Social {
-			site {
-				siteMetadata {
-					social {
-						github
-						twitter
-						linkedIn
-					}
-				}
-			}
-		}
-	`);
+  const data: SocialQuery = useStaticQuery(graphql`
+    query Social {
+      site {
+        siteMetadata {
+          social {
+            github
+            twitter
+            linkedIn
+          }
+        }
+      }
+    }
+  `);
 
-	const social = data.site.siteMetadata.social;
+  const social = data.site.siteMetadata.social;
 
-	return (
-		<big>
-			<a
-				className="social-link no-invert"
-				href={`https://www.github.com/${social.github}`}
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<FontAwesomeIcon title="Github" icon={faGithub} width={18} />
-			</a>{" "}
-			<a
-				className="social-link no-invert"
-				href={`https://www.linkedin.com/in/${social.linkedIn}`}
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<FontAwesomeIcon title="LinkedIn" icon={faLinkedin} width={18} />
-			</a>{" "}
-			<a
-				className="social-link no-invert"
-				href={`https://www.twitter.com/${social.twitter}`}
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<FontAwesomeIcon title="Twitter" icon={faTwitter} width={18} />
-			</a>
-		</big>
-	);
+  return (
+    <big>
+      <a
+        className="social-link no-invert"
+        href={`https://www.github.com/${social.github}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaGithub title="Github" width={18} />
+      </a>{" "}
+      <a
+        className="social-link no-invert"
+        href={`https://www.linkedin.com/in/${social.linkedIn}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaLinkedin title="LinkedIn" width={18} />
+      </a>{" "}
+      <a
+        className="social-link no-invert"
+        href={`https://www.twitter.com/${social.twitter}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaTwitter title="Twitter" width={18} />
+      </a>
+    </big>
+  );
 }
